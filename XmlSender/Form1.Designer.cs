77773e3cdc9a3a78f9ac34d89ec4e56559809db0
������ <amatselya@gmail.com>
2014-07-02 08:59:04 +0300

    sdf

--- XmlSender/Form1.Designer.cs
+++ XmlSender/Form1.Designer.cs
@@ -29,6 +29,10 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.историяОперацийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -41,21 +45,53 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.историяОперацийToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(537, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.fileToolStripMenuItem.Text = "Файл";
+			this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+			// 
+			// историяОперацийToolStripMenuItem
+			// 
+			this.историяОперацийToolStripMenuItem.Name = "историяОперацийToolStripMenuItem";
+			this.историяОперацийToolStripMenuItem.Size = new System.Drawing.Size(123, 20);
+			this.историяОперацийToolStripMenuItem.Text = "История операций";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(537, 374);
 			this.Controls.Add(this.button1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem историяОперацийToolStripMenuItem;
 	}
 }
 
--- XmlSender/Form1.cs
+++ XmlSender/Form1.cs
@@ -3,26 +3,248 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Security.Tokens;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 using XmlSender.ServiceReference1;
+using XmlSender.ServiceReference3;
+using Message = System.ServiceModel.Channels.Message;
 
 namespace XmlSender
 {
+	public class UsernameToken : MessageHeader
+	{
+		public string Username
+		{
+			get
+			{
+				return FUsername;
+			}
+			set
+			{
+				FUsername = value;
+			}
+		}
+		public string Password
+		{
+			get
+			{
+				return FPassword;
+			}
+			set
+			{
+				FPassword = value;
+			}
+		}
+		private string FUsername = String.Empty;
+		private string FPassword = String.Empty;
+
+		public override bool MustUnderstand
+		{
+			get { return true; }
+		}
+
+		public override string Name
+		{
+			get { return "Security"; }
+		}
+
+		public override string Namespace
+		{
+			get { return "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"; }
+		}
+
+		protected override void OnWriteHeaderContents(XmlDictionaryWriter writer, MessageVersion messageVersion)
+		{
+			writer.WriteStartElement("UsernameToken", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+			writer.WriteAttributeString("Id", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "UsernameToken-1");
+
+			writer.WriteElementString("Username", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd", this.Username);
+			writer.WriteStartElement("Password", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+			writer.WriteAttributeString("Type", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText");
+			writer.WriteValue(this.Password);
+			writer.WriteEndElement();
+			writer.WriteEndElement();
+		}
+	}
+
+	public class CustomMessageInspector : IClientMessageInspector
+	{
+		public object BeforeSendRequest(ref Message request, IClientChannel channel)
+		{
+			var header = new UsernameToken()
+			{
+				Username = "dimex",
+				Password = "q"
+			};
+			
+			MessageBuffer buffer = request.CreateBufferedCopy(Int32.MaxValue);
+			request = buffer.CreateMessage();
+			request.Headers.Add(header);
+			return null;
+		}
+
+		public void AfterReceiveReply(ref Message reply, object correlationState)
+		{
+			//throw new NotImplementedException();
+		}
+
+	}
+
+	[AttributeUsage(AttributeTargets.Class)]
+	public class CustomBehavior : Attribute, IEndpointBehavior
+	{
+		#region IEndpointBehavior Members
+
+		public void AddBindingParameters(ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+		{
+		}
+
+		public void ApplyClientBehavior(ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.ClientRuntime clientRuntime)
+		{
+			CustomMessageInspector inspector = new CustomMessageInspector();
+			clientRuntime.MessageInspectors.Add(inspector);
+		}
+
+		public void ApplyDispatchBehavior(ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.EndpointDispatcher endpointDispatcher)
+		{
+			//ChannelDispatcher channelDispatcher = endpointDispatcher.ChannelDispatcher;
+			//if (channelDispatcher != null)
+			//{
+			//	foreach (EndpointDispatcher ed in channelDispatcher.Endpoints)
+			//	{
+			//		CustomMessageInspector inspector = new CustomMessageInspector();
+			//		ed.DispatchRuntime.MessageInspectors.Add(inspector);
+			//	}
+			//}
+		}
+
+		public void Validate(ServiceEndpoint endpoint)
+		{
+		}
+
+		#endregion
+	}
+
+	public class CustomBehaviorExtensionElement : BehaviorExtensionElement
+	{
+		protected override object CreateBehavior()
+		{
+			return new CustomBehavior();
+		}
+
+		public override Type BehaviorType
+		{
+			get
+			{
+				return typeof(CustomBehavior);
+			}
+		}
+	} 
+
 	public partial class Form1 : Form
 	{
 		public Form1()
 		{
 			InitializeComponent();
+			//var list = new List<insurance_data_request>();
+			//list.Add(new insurance_data_request()
+			//{
+			//	cover = new ServiceReference3.MessageCover() { message_source = new ServiceReference3.Classifier() }
+			//});
+			//var ser = new XmlSerializer(typeof(List<insurance_data_request>));
+			//using (var writer = XmlWriter.Create("filex.xml"))
+			//{
+			//	ser.Serialize(writer,list);
+			//}
 		}
+
+		private List<insurance_data_request> file;
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			var client = new ServiceReference1.GisunCommonWsImplClient();
-			//client.getPersonIdentif();
-			ServiceReference1.RequestData.Create
+
+
+			var client = new ServiceReference3.InsuranceWSClient("InsuranceWS");
+			MessageHeader.CreateHeader("dfg", "sdfg", new {dsfgs = "dfg"});
+
+			//using (new OperationContextScope(client.InnerChannel))
+			//{
+
+			//	// We will use a custom class called UserInfo to be passed in as a MessageHeader
+
+
+
+
+
+			//	// Add a SOAP Header to an outgoing request
+
+			//	var aMessageHeader = MessageHeader.CreateHeader("Security", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd", header);
+
+			//	OperationContext.Current.OutgoingMessageHeaders.Add(aMessageHeader);
+
+
+
+			//}
+			var response = client.postInsuranceData(new insurance_data_request()
+			{
+				cover = new ServiceReference3.MessageCover()
+				{
+					message_id = Guid.NewGuid().ToString(),
+					message_time = DateTime.Now
+				}
+			});
+		}
+
+
+		private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Stream myStream = null;
+
+
+			var openFileDialog1 = new OpenFileDialog();
+
+			//openFileDialog1.InitialDirectory = "c:\\";
+			openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+			openFileDialog1.FilterIndex = 1;
+			openFileDialog1.RestoreDirectory = true;
+
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				try
+				{
+					if ((myStream = openFileDialog1.OpenFile()) != null)
+					{
+						using (myStream)
+						{
+							var serializer = new XmlSerializer(typeof(List<insurance_data_request>), new XmlRootAttribute("root"));
+							serializer.UnknownElement += serializer_UnknownElement;
+							file = (List<insurance_data_request>)serializer.Deserialize(myStream);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+				}
+			}
+		}
+
+		void serializer_UnknownElement(object sender, XmlElementEventArgs e)
+		{
+			var t = e.Element;
 		}
 	}
 }
